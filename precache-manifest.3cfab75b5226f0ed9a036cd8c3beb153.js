388e56a2fde1ff33d25d6dda195df472d314503a
coderyogesh <yogeshrj1433@gmail.com>
2023-11-07 23:22:21 +0530

    Deploy React app to GitHub Pages

---
+++ precache-manifest.3cfab75b5226f0ed9a036cd8c3beb153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6b5c220613c5017ab44d40212472c5",
+    "url": "/form/index.html"
+  },
+  {
+    "revision": "4bdac18922326bdc4fe2",
+    "url": "/form/static/css/main.de751b18.chunk.css"
+  },
+  {
+    "revision": "97ce4cbe280abe88f8db",
+    "url": "/form/static/js/2.09f26f7e.chunk.js"
+  },
+  {
+    "revision": "0eb3ca9d0061837f6c8f",
+    "url": "/form/static/js/3.31c88699.chunk.js"
+  },
+  {
+    "revision": "4bdac18922326bdc4fe2",
+    "url": "/form/static/js/main.cf3780bd.chunk.js"
+  },
+  {
+    "revision": "723c634bff8658895b69",
+    "url": "/form/static/js/runtime~main.8c826391.js"
+  }
+]);
